c0c4548a23c0244348cb630c95268947532e0197
unknown <mkashifx6@gmail.com>
2023-05-05 16:55:04 +0500

    Nav fixed

--- src/Components/Common/Sidebar/Sidebar.js
+++ src/Components/Common/Sidebar/Sidebar.js
@@ -17,9 +17,9 @@ const Sidebar = () => {
         class="fixed top-0 left-0 z-40 w-[250px] h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div class="h-full flex flex-col justify-between px-3 py-4 overflow-y-auto bg-[#0E141B]">
-          <ul class="space-y-2 text-normal	font-bold	">
-            <li>
+        <div class="h-full flex flex-col justify-between px-3 py-4  bg-[#0E141B]">
+          <ul class=" text-normal	font-bold	">
+            <li className="mt-[10px]">
               <Link
                 to="/"
                 className={`flex items-center p-2 text-[#FFFFFF] rounded-lg hover:bg-blue-800 ${isActive(
@@ -30,7 +30,7 @@ const Sidebar = () => {
                 <span class="ml-3">Home</span>
               </Link>
             </li>
-            <li>
+            <li className="mt-[10px]">
               <Link
                 to="/applications"
                 className={`flex items-center p-2 text-[#FFFFFF] rounded-lg hover:bg-blue-800 ${isActive(
@@ -41,7 +41,7 @@ const Sidebar = () => {
                 <span class="ml-3">Applications</span>
               </Link>
             </li>
-            <li>
+            <li className="mt-[10px]">
               <Link
                 to="/media"
                 className={`flex items-center p-2 text-[#FFFFFF] rounded-lg hover:bg-blue-800 ${isActive(
@@ -52,7 +52,7 @@ const Sidebar = () => {
                 <span class="ml-3">Media</span>
               </Link>
             </li>
-            <li>
+            <li className="mt-[10px]">
               <Link
                 to="/downloads"
                 className={`flex items-center p-2 text-[#FFFFFF] rounded-lg hover:bg-blue-800 ${isActive(
@@ -63,7 +63,7 @@ const Sidebar = () => {
                 <span class="ml-3">Downloads</span>
               </Link>
             </li>
-            <li className="">
+            <li className="mt-[350px]">
               <Link
                 to="/settings"
                 className={`flex items-center p-2 text-[#FFFFFF] rounded-lg hover:bg-blue-800 ${isActive(
